b65692224ca2b2477d9dbbe55f533824b18b4278
beatbox4108 <68136638+beatbox4108@users.noreply.github.com>
2022-08-21 15:58:11 +0900

    some changes

--- build/build.ps1
+++ build/build.ps1
@@ -1,4 +1,5 @@
 ./venv/Scripts/Activate.ps1
-pip install --upgrade pip setuptools wheel
-python ./src/setup.py bdist_wheel
-python ./src/setup.py sdist
+pip3 install --upgrade pip setuptools wheel
+cd ./playquick
+python ./setup.py bdist_wheel
+python ./setup.py sdist
